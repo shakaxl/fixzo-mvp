03a9ca3bb22f962c1f561ebd7f7df95e4b3de106
Williams Padilla <wpadilla007@gmail.com>
2025-10-27 21:51:23 -0500

    Agregado Meta Pixel para tracking de campañas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,30 @@ export default function RootLayout({
             gtag('config', 'G-L5QTMP9WG8');
           `}
         </Script>
+        {/* Meta Pixel */}
+    <Script id="meta-pixel" strategy="afterInteractive">
+      {`
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1462958004777902');
+        fbq('track', 'PageView');
+      `}
+    </Script>
+    <noscript>
+      <img 
+        height="1" 
+        width="1" 
+        style={{display: 'none'}}
+        src="https://www.facebook.com/tr?id=1462958004777902&ev=PageView&noscript=1"
+        alt=""
+      />
+    </noscript>
       </head>
       <body>{children}</body>
     </html>
